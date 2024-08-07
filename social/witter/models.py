1caafbfc200a665fd1c384b88a141fd54e8dff57
Chris Borowski <krzysztofeborowski@gmail.com>
2024-08-07 00:20:03 -0500

    Add option to create user profiles and define user-to-user relationships

--- social/witter/models.py
+++ social/witter/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Create a user profile model
+
+class UserProfile(models.Model):
+
+  # Set a one-to-one relationship between each user and their profile
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  
+  # Allow for many-to-many relationships between profiles
+  follows = models.ManyToManyField("self",
+  related_name="followed_by",
+  symmetrical=False, # Set asymmetrical user-to-user following relationship
+  blank=True) # Allow user not to follow anyone
+
+  def __str__(self):
+    return self.user.username
