d73b7cdac40eb5775ca655908ca2cc0a8a33f6f9
Chris Borowski <krzysztofeborowski@gmail.com>
2024-08-06 23:52:33 -0500

    Unregister the initial User and reregister the User as UserAdmin

--- social/witter/admin.py
+++ social/witter/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, User
 
 # Unregistering groups
 admin.site.unregister(Group)
+
+# Extending User model
+class UserAdmin(admin.ModelAdmin):
+  model = User
+
+  fields = ["username"]
+
+# Unregistering initial User
+admin.site.unregister(User)
+
+# Reregistering User
+admin.site.register(User, UserAdmin)
