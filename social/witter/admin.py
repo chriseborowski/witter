88d3ecda2f431b83c93fa244352b7de12f2c14c3
Chris Borowski <krzysztofeborowski@gmail.com>
2024-08-07 00:21:12 -0500

    Add support for connecting user profiles to users

--- social/witter/admin.py
+++ social/witter/admin.py
@@ -1,17 +1,25 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, User
+from .models import UserProfile
 
 # Unregistering groups
 admin.site.unregister(Group)
+
+# Connect user profiles to users
+class ProfileInline(admin.StackedInline):
+  model = UserProfile
 
 # Extending User model
 class UserAdmin(admin.ModelAdmin):
   model = User
 
   fields = ["username"]
+  inlines = [ProfileInline]
 
 # Unregistering initial User
 admin.site.unregister(User)
 
-# Reregistering User
+# Reregistering User and UserProfile
 admin.site.register(User, UserAdmin)
+# admin.site.register(UserProfile)
+
