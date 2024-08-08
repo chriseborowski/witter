dee2c17388fa05ea3fa177d93e67bb38cc6cca12
Chris Borowski <krzysztofeborowski@gmail.com>
2024-08-07 23:28:10 -0500

    Add support for displaying Profile list element

--- social/witter/views.py
+++ social/witter/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import UserProfile
 
 # Create your views here.
 def home(request):
   return render(request, 'home.html', {})
+
+def profile_list(request):
+  profiles = UserProfile.objects.exclude(user=request.user)
+
+  return render(request, 'profile_list.html', {"profiles": profiles})
