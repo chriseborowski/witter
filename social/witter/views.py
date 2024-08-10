db1b9589bfc64e341153bf75d84c320b81af9b2b
Chris Borowski <krzysztofeborowski@gmail.com>
2024-08-10 10:58:07 -0500

    Add Follow/Unfollow logic

--- social/witter/views.py
+++ social/witter/views.py
@@ -17,6 +17,16 @@ def profile_list(request):
 def profile(request, pk):
   if request.user.is_authenticated:
     profile = UserProfile.objects.get(user_id=pk)
+
+    # Follow/Unfollow logic using POST
+    if request.method == "POST":
+      current_user_profile = request.user.profile
+      action = request.POST['follow']
+      if action == "unfollow":
+        current_user_profile.follows.remove(profile)
+      else:
+        current_user_profile.follows.add(profile)
+    
     return render(request, 'profile.html', {"profile": profile})
   else:
     messages.success(request, ("You must be logged in to view this page"))
