6d73f32d7c2682480b3764fa8d4f90d8e92559e4
Chris Borowski <krzysztofeborowski@gmail.com>
2024-08-19 12:05:12 -0500

    Add user model import

--- social/witter/views.py
+++ social/witter/views.py
@@ -4,6 +4,7 @@ from .models import UserProfile, Witt
 from .forms import WittForm, SignUpForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django import forms
 
 # Create your views here.
