5c1803634604ae5efe60bda930de07808e681f83
Chris Borowski <krzysztofeborowski@gmail.com>
2024-08-18 22:19:53 -0500

    Add login logic to login_user function with automatically generated success/error messages

--- social/witter/views.py
+++ social/witter/views.py
@@ -53,7 +53,21 @@ def profile(request, pk):
 
 
 def login_user(request):
-  return render(request, 'login.html', {})
+  if request.method == "POST":
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None:
+      login(request, user)
+      messages.success(request, ("You have been logged in."))
+      return redirect('home')
+    else:
+      messages.success(request, ("There was an error logging in. Please try again."))
+      return redirect('login')
+
+  else:
+    return render(request, 'login.html', {})
 
 
 def logout_user(request):
