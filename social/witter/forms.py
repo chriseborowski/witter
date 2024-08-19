584533a58a7474cea668f260f20d7c0ff5e25002
Chris Borowski <krzysztofeborowski@gmail.com>
2024-08-19 12:27:55 -0500

    Add ProfileUpdateFormclass to handle updating user profile

--- social/witter/forms.py
+++ social/witter/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Witt
+from .models import Witt, UserProfile
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
@@ -29,7 +29,7 @@ class SignUpForm(UserCreationForm):
     fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
 
   def __init__(self, *args, **kwargs):
-    super(SignUpForm, self).__init__(*args, **kwargs)
+    self(superSignUpForm, self).__init__(*args, **kwargs)
 
     self.fields['username'].widget.attrs['class'] = 'form-control'
     self.fields['username'].widget.attrs['placeholder'] = 'Your username'
@@ -45,3 +45,17 @@ class SignUpForm(UserCreationForm):
     self.fields['password2'].widget.attrs['placeholder'] = 'Confirm your password'
     self.fields['password2'].label = ''
     self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'
+
+
+class ProfileUpdateForm(forms.ModelForm):
+  email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder':'Your email address'}))
+  first_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Your first name'}))
+  last_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Your last name'}))
+
+  class Meta:
+    model = User
+    fields = ('first_name', 'last_name', 'email')
+
+  def __init__(self, *args, **kwargs):
+    super(ProfileUpdateForm, self).__init__(*args, **kwargs)
+    self.fields['email'].label = "Email address"
