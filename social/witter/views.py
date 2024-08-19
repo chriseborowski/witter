3ebb23c294bdbf23572452749d0e932a101d67e3
Chris Borowski <krzysztofeborowski@gmail.com>
2024-08-19 11:18:26 -0500

    Add signup logic to register_user function

--- social/witter/views.py
+++ social/witter/views.py
@@ -79,4 +79,21 @@ def logout_user(request):
 
 
 def register_user(request):
-  return render(request, 'register.html', {})
+  form = SignUpForm()
+  if request.method == "POST":
+    form = SignUpForm(request.POST)
+    if form.is_valid():
+      form.save()
+      username = form.cleaned_data['username']
+      password = form.cleaned_data['password1']
+      # first_name = form.cleaned_data['first_name']
+      # last_name = form.cleaned_data['last_name']
+      # email = form.cleaned_data['email']
+      # Log in user
+      user = authenticate(username=username, password=password)
+      login(request, user)
+      messages.success(request, ("You have successfully registered. Welcome to Witter!"))
+      return redirect('home')
+  else:
+    form = SignUpForm()
+  return render(request, 'register.html', {'form': form})
