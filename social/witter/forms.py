56fb98f1da134c5ffd2bdc8ff7ecf0a017825b54
Chris Borowski <krzysztofeborowski@gmail.com>
2024-08-19 10:54:53 -0500

    Add Django user authentication imports

--- social/witter/forms.py
+++ social/witter/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Witt
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class WittForm(forms.ModelForm):
     body = forms.CharField(required=True,
