2b163ab6de92d9edfb28a9b0981e0234d76ec640
Chris Borowski <krzysztofeborowski@gmail.com>
2024-08-25 12:30:14 -0500

    Add search_user function to manage user search

--- social/witter/views.py
+++ social/witter/views.py
@@ -191,3 +191,12 @@ def search(request):
     return render(request, 'search.html', {'search': search, 'searched': searched})
   else:
     return render(request, 'search.html', {})
+
+
+def search_user(request):
+  if request.method == "POST":
+    search = request.POST['search']
+    searched = User.objects.filter(username__contains=search)
+    return render(request, 'search_user.html', {'search': search, 'searched': searched})
+  else:
+    return render(request, 'search_user.html', {})
